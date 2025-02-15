df70a3e4b9fd9eb14c19e52462f2b15957252b2f
Srivatsan <talkads123@gmail.com>
2025-02-15 03:53:34 +0000

    create getDescription override

--- Car.hpp
+++ Car.hpp
@@ -11,6 +11,10 @@ public:
 
     // Getter
     int getDoors() const;
+
+    // Override getDescription() of parent class
+    const std::string& getDescription() const override;
+
 private:
     int doors;
 };
