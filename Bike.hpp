ef5fb982ad748222b82fcb2f86c12d28f201340e
Srivatsan <talkads123@gmail.com>
2025-02-16 04:37:14 +0000

    create getDescription override

--- Bike.hpp
+++ Bike.hpp
@@ -8,6 +8,10 @@ public:
     // Constructors
     Bike();
     Bike(const std::string& brand, bool hasGears);
+
+    // Override getDescription() of parent class
+    const std::string& getDescription() const override;
+    
 private:
     bool gears;
 }
