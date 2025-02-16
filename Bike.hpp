9d650142ab15f94494dbe8f328b0355b914879f0
Srivatsan <talkads123@gmail.com>
2025-02-16 04:38:24 +0000

    create getter

--- Bike.hpp
+++ Bike.hpp
@@ -9,9 +9,12 @@ public:
     Bike();
     Bike(const std::string& brand, bool hasGears);
 
+    // Getter
+    bool hasGears() const;
+
     // Override getDescription() of parent class
     const std::string& getDescription() const override;
-    
+
 private:
     bool gears;
 }
